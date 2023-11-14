bf9ccc5f861ee0f29e24647dc9d6dc3f4e1a2285
AlulCode45 <alulgans70@gmail.com>
2023-11-14 14:18:13 +0700

    Changes preloader: 	modified:   assets/image/loading.gif 	modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var preloader = document.querySelector(".preloader");
     setTimeout(function () {
         preloader.style.display = "none";
-    }, 300);
+    }, 1700);
 });
 
 var comments = document.getElementsByClassName('comment');
